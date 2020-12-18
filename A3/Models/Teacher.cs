a72f3fd8ac50b9f60f43aecd2c73cf6cdf842853
naihsienchen <nhc0629@gmail.com>
2020-12-17 22:40:29 -0500

    update valitaion method

--- A3/Models/Teacher.cs
+++ A3/Models/Teacher.cs
@@ -16,30 +16,30 @@ namespace A3.Models
         public string TeacherLname;
         public string TeacherInfo;
         public string EmployeeNumber;
-    }
+    
 
-    public bool IsValid()
-    {
-        bool valid = true;
-
-        if (TeacherFname == null || TeacherLname == null || TeacherInfo == null || EmployeeNumber == null)
+        public bool IsValid()
         {
-            //Base validation to check if the fields are entered.
-            valid = false;
-        }
-        else
-        {
-            //Validation for fields to make sure they meet server constraints
-            if (TeacherFname.Length < 2 || TeacherFname.Length > 255) valid = false;
-            if (TeacherLname.Length < 2 || TeacherLname.Length > 255) valid = false;
-            //C# email regex 
-            //https://stackoverflow.com/questions/5342375/regex-email-validation
-            Regex ENumberVal = new Regex(@"(T|t)\d{4}");
-            if (!ENumberVal.IsMatch(EmployeeNumber)) valid = false;
-        }
-        Debug.WriteLine("The model validity is : " + valid);
+            bool valid = true;
 
-        return valid;
+            if (TeacherFname == null || TeacherLname == null || TeacherInfo == null || EmployeeNumber == null)
+            {
+                //Base validation to check if the fields are entered.
+                valid = false;
+            }
+            else
+            {
+                //Validation for fields to make sure they meet server constraints
+                if (TeacherFname.Length < 2 || TeacherFname.Length > 255) valid = false;
+                if (TeacherLname.Length < 2 || TeacherLname.Length > 255) valid = false;
+                //C# email regex 
+                //https://stackoverflow.com/questions/5342375/regex-email-validation
+                Regex ENumberVal = new Regex(@"(T|t)\d{4}");
+                if (!ENumberVal.IsMatch(EmployeeNumber)) valid = false;
+            }
+            Debug.WriteLine("The model validity is : " + valid);
+
+            return valid;
+        }
     }
-
 }
